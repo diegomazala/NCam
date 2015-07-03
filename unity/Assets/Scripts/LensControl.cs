b0191045e57ed4da1b49ff54e2c02ad58eb1680c
Diego Mazala <diegomazala@gmail.com>
2015-07-03 10:56:27 -0300

    Level criado com um exemplo básico de integração do LensEncoder com o LensTable

---
+++ unity/Assets/Scripts/LensControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LensControl : MonoBehaviour 
+{
+    public LensTable lensTable;
+    public LensEncoder lensEncoder;
+
+
+	void OnEnable() 
+    {
+        if (lensTable == null)
+            lensTable = FindObjectOfType<LensTable>();
+
+
+        if (lensTable == null)
+        {
+            Debug.LogError("Could not find LensTable object. LensControl will be disabled.");
+            enabled = false;
+            return;
+        }
+
+
+        if (lensEncoder == null)
+            lensEncoder = FindObjectOfType<LensEncoder>();
+
+
+        if (lensEncoder == null)
+        {
+            Debug.LogError("Could not find LensEncoder object. LensControl will be disabled.");
+            enabled = false;
+            return;
+        }
+	}
+	
+
+
+	void Update () 
+    {
+        lensTable.UpdateCameraLens(lensEncoder.ZoomNormalized, lensEncoder.FocusNormalized);
+	}
+}
